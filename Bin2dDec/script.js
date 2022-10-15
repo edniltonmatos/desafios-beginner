b724962db4981559398c1a10764e354087743254
edniltonmatos <edniltonproframador@gmail.com>
2022-10-15 06:37:34 -0300

    realizado no termux

--- Bin2dDec/script.js
+++ Bin2dDec/script.js
@@ -1,2 +1,24 @@
-const Inf = document.getElementById("inf")
-const binString = document.getElementById("bin-string")
+const num = document.querySelector("input")
+number = 0
+let result = 0
+let resp = document.querySelector("#res")
+
+num.addEventListener("input",updateValue)
+numArray = String(number).split("").map((number) => {
+    return Number(number)
+  })
+  
+arrayInvert = numArray.reverse()
+
+function bin2dec(item, index, arr){
+  result += item * 2**index
+}
+
+arrayInvert.forEach(bin2dec)
+
+function updateValue(e){
+  number = e.target.value
+}
+resp.innerHTML = result
+
+
