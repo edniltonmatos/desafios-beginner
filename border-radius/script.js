4c5863e4aeec9e0c14ef03801bdb1ba55ed1681d
Ednilton Matos <edniltonprogramador@gmail.com>
2022-11-02 16:52:57 -0300

    :construction: Border Radius: add cover

--- border-radius/script.js
+++
@@ -1,8 +0,0 @@
-const input1 = document.getElementById("input-p1")
-const topLeft = document.getElementById("top-left")
-const container = document.getElementById("container-border-radius")
-
-input1.addEventListener("focus",add)
-function add (){
-    topLeft.innerHTML = input1.value
-}
