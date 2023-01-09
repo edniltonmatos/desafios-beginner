f8a91aef97bc78fde4a00193f4f4c668b98bf8ef
Ednilton Matos <edniltonprogramador@gmail.com>
2023-01-09 09:33:02 -0300

    :construction: javaScript: add retordo usuário

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,11 +1,68 @@
-input = document.getElementById("input")
-numbers = document.querySelectorAll(".numbers")
-let calc = 0
+let input = document.getElementById("input")
 
+let zero = document.getElementById("0")
+let um = document.getElementById("1")
+let dois = document.getElementById("2")
+let tres = document.getElementById("3")
+let quatro = document.getElementById("4")
+let cinco = document.getElementById("5")
+let seis = document.getElementById("6")
+let sete = document.getElementById("7")
+let oito = document.getElementById("8")
+let nove = document.getElementById("9")
 
-numbers.forEach((item,index,arr) => {
-    arr[index].value = index + 1
-    item.addEventListener("click", () => {
-        input.innerHTML = this.value
-    })
-});
+let igual = document.getElementById("igual")
+let maisOuMenos = document.getElementById("+/-")
+let AC = document.getElementById("AC")
+let C = document.getElementById("C")
+let mais = document.getElementById("+")
+let menos = document.getElementById("-")
+let dividido = document.getElementById("/")
+let vezes = document.getElementById("x")
+let ponto = document.getElementById(".")
+
+AC.addEventListener("click", () =>{
+    input.innerHTML = "0"
+})
+vezes.addEventListener("click", () =>{
+    input.innerHTML += "x"
+})
+dividido.addEventListener("click", () =>{
+    input.innerHTML += "/"
+})
+menos.addEventListener("click", () =>{
+    input.innerHTML += "-"
+})
+mais.addEventListener("click", () =>{
+    input.innerHTML += "+"
+})
+zero.addEventListener("click", () =>{
+    input.innerHTML += "0"
+})
+um.addEventListener("click", () =>{
+    input.innerHTML += "1"
+})
+dois.addEventListener("click", () =>{
+    input.innerHTML += "2"
+})
+tres.addEventListener("click", () =>{
+    input.innerHTML += "3"
+})
+quatro.addEventListener("click", () =>{
+    input.innerHTML += "4"
+})
+cinco.addEventListener("click", () =>{
+    input.innerHTML += "5"
+})
+seis.addEventListener("click", () =>{
+    input.innerHTML += "6"
+})
+sete.addEventListener("click", () =>{
+    input.innerHTML += "7"
+})
+oito.addEventListener("click", () =>{
+    input.innerHTML += "8"
+})
+nove.addEventListener("click", () =>{
+    input.innerHTML += "9"
+})
